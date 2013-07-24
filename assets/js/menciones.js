61415285be6f9c70a0e9894c7c70e9c014999f0a
e-9 <abraham@codefire.mx>
2013-07-23 23:19:12 -0500

    Muestra tabla con las ultimas 20 menciones

--- assets/js/menciones.js
+++ assets/js/menciones.js
@@ -1,44 +1,18 @@
+
 var cb = new Codebird;
 cb.setConsumerKey("8yzY7Zgs01XjibpG7XceBQ", "IDjsaIM6Q34M2OIwIHm8Bc9KEQHBRxDqoAEB81IgQuk");
 cb.setToken("88465140-dPpEMQXl9n5wBFZTLVhLRR21XL4YHT7frDlrSQ46I", "XaeolpyyfydVTTlhQygZofJyCsA8rnWc3MwZmNDxNM");
-
 cb.__call(
     "statuses_mentionsTimeline",
     {},
     function (reply) {
         console.log(reply);
+
+        var App = Ember.Application.create();
+        App.ApplicationRoute = Ember.Route.extend({
+          model: function() {
+            return reply;
+          }
+        });
     }
 );
-
-
-var users = [
-  {
-    idTwtt: 1,
-    idUsr: 88,
-    name: 'Ryan',
-    twitt: 'estos son casi 140 caracteres',
-    status: "Pendiente"
-  },
-  {
-    idTwtt: 2,
-    idUsr: 89,
-    name: 'Tom',
-    twitt: 'hola mundo cat',
-    status: "Pendiente"
-  },
-  {
-    idTwtt: 3,
-    idUsr: 90,
-    name: 'Yehuda',
-    twitt: 'abcdefghijklmnopqrstuvwxyz',
-    status: "Pendiente"
-  }
-];
-
-var App = Ember.Application.create();
-
-App.ApplicationRoute = Ember.Route.extend({
-  model: function() {
-    return users;
-  }
-});
